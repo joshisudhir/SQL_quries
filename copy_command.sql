405b3f7510d4e1aadcdd113a1945a3a78a1726fe
sudhir joshi <sudhirjoshi509@gmail.com>
2024-08-23 13:50:39 +0530

    comment

--- copy_command.sql
+++ copy_command.sql
@@ -1,6 +1,6 @@
 COPY  your.table.name
 FROM 's3://bucket-name/folder/file.csv' --s3path
-IAM_ROLE 'EnterIAMROLE----GO to redshift cluster there you will find it in properties'
+IAM_ROLE 'EnterIAMROLE----GO to redshift cluster there you will find it in properties' --iam role need 
 DELIMITER ','  --csv delimiter
 IGNOREHEADER 1 --for ignoring header
 DATEFORMAT 'YYYY-MM-DD'-- may be auto as well
